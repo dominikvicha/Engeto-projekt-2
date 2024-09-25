030aa584f446dbddc3dca32c7a4b38da19e450f7
dominikvicha <dominikvicha142@gmail.com>
2024-09-25 17:26:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-# Tic-tac-toe Game 
+"""
+Tic-tac-toe Game
+projekt_2: druhý projekt do Engeto Online Python Akademie
+
+autor: Vícha Dominik
+email: dominik.vicha@gmail.com 
+discord: Dominik V
+
+"""
+
+
+"""
+ZADÁNÍ: 
+2. program pozdraví uživatele,
+3. vypíše v krátkosti pravidla hry,
+4. zobrazí hrací plochu,
+5. vyzve prvního hráče, aby zvolil pozici pro umístění hracího kamene,
+6. pokud hráč zadá jiné číslo, než je nabídka hracího pole, program jej upozorní,
+7. pokud hráč zadá jiný vstup, než číselný, program jej opět upozorní,
+8. pokud se na vybraném poli nachází hrací kámen druhého hráče, program jej upozorní, že je pole obsazené
+9. jakmile hráč úspěšně vybere pole, zobrazíme nový stav hrací plochy,
+10. program vyhodnocuje, jestli horizontálně/vertikálně/diagonálně není některý hrací kámen tříkrát. Pokud ano, vyhrává hráč, kterému tyto tři kameny patří,
+11. pokud nezbývá žádné volné hrací pole a žádný hráč nevyhrál, jde o remízu,
+12. zápis organizovaný do krátkých a přehledných funkcí.
+
+"""
+
+
